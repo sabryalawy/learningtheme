011bf403190accb84fe9594d806238f769f33057
Sabry alawy <sabry0595911611@gmail.com>
2021-02-20 01:04:26 +0200

    scrach

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/* 
+ *this will be a git repo 
+ * 
+ * learning themes
+ *  
+ */
+
+?>
+<html>
+    
+    <head>
+        
+    </head>
+    <body>
+        <p>body</p><br>
+    </body>
+
+</html>
+
